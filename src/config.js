a7a310449fcafc7c2d245d2fcb04a03b59bf1bed
Jun <jyin25@gmail.com>
2019-11-09 11:10:00 -0800

    update authentication

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DATABASE_URL || 'postgresql://FSE_admin@localhost/FSE_Server'
+    DB_URL: process.env.DATABASE_URL || 'postgresql://fse_admin@localhost/fse_Server',
+    JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret'
   }
--- src/equity/equity-router.js
+++ src/equity/equity-router.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const equityService = require('./equity-service')
+const {requireAuth} = require('../auth/jwt-auth')
 
 const equityRouter = express.Router()
 const jsonBodyParser = express.json()
 
 equityRouter
   .route('/')
+  .all(requireAuth)
   .post(jsonBodyParser, (req, res, next) => {
     const db = req.app.get('db')
-    console.log(req.body.stock_symbol)
     const {userid, stock_symbol, num_of_shares, groupid} = req.body
     const addEquity = {
       userid,
@@ -26,10 +27,10 @@ equityRouter
 
 equityRouter
   .route('/:groupid')
+  .all(requireAuth)
   .get((req, res, next) => {
     const db = req.app.get('db')
-    // const id = req.user.id
-    const id = 1
+    const id = req.user.id
     const groupid = req.params.groupid
     equityService.getEquity(db, id, groupid)
       .then(data => {
