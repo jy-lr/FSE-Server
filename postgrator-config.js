958b4dd52fab67f02a5fbe2d5364e40da1525284
Lance Russell <lancebryanrussell@gmail.com>
2019-11-21 22:13:49 -0600

    depl

--- postgrator-config.js
+++ postgrator-config.js
@@ -8,7 +8,7 @@ module.exports = {
     : process.env.DATABASE_URL,
   "host": process.env.MIGRATION_DB_HOST,
   "port": process.env.MIGRATION_DB_PORT,
-  "database": process.env.MIGRATION_DB_NAME,
+  "database": 'fse_Server',
   "username": process.env.MIGRATION_DB_USER,
   "password": process.env.MIGRATION_DB_PASS,
   "ssl": !!process.env.SSL,
